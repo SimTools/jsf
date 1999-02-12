0abe855d466f387ac0e57ae46d930c91ab33205f
Akiya Miyamoto <akiya.miyamoto@kek.jp>
1999-02-12 08:49:57 +0000

    12-Feburary-1999 A.Miyamoto    Sources for debug.

--- example/FFbarSpring/read.C
+++ example/FFbarSpring/read.C
@@ -5,7 +5,7 @@
 
   jsf->Initialize();
 
-  Int_t maxevt=10;
+  Int_t maxevt=30;
 
   JSFQuickSim *sim=jsf->FindModule("JSFQuickSim");
   JSFQuickSimBuf *sbuf=(JSFQuickSimBuf*)sim->EventBuf();
@@ -13,14 +13,22 @@
   JSFHadronizer *had=(JSFHadronizer*)jsf->FindModule("JSFHadronizer");
   FFbarSpring *spr=(FFbarSpring*)jsf->FindModule("FFbarSpring");
 
+  TBranch *br=sim->GetBranch();
+  printf(" br=%d\n",(Int_t)br);
   for(Int_t i=1;i<=maxevt;i++){
     //    if( !jsf->GetEvent(i) ) break;
     //    if( !spr->GetBranch()->GetEvent(i) ) break;
-    sbuf->GetEventTracks(i);
-    sim->GetBranch()->GetEvent(i);
+    // sbuf->GetEventTracks(i);
+
+    //    jsf->ITree()->Print();
+
+    if( !br->GetEvent(i) ) break;
 
     printf(" Get event #%d\n",i);
     printf(" sbuf->Ntrack is %d\n",sbuf->GetNTracks());
+    printf(" sbuf->GetNVTXHits=%d\n",sbuf->GetNVTXHits());
+    printf(" sbuf is %d\n",(Int_t)sbuf);
+    sbuf->GetEventTracks(i);
     
     //    jsf->ITree()->Print();
     //    if( !jsf->Process(i) ) break ;
