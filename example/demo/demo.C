59402e432b133b4c696f1ad47a121a01e63fa7b2
Akiya Miyamoto <akiya.miyamoto@kek.jp>
1999-08-27 00:43:08 +0000

    27-August-1999 Akiya Miyamoto   Script and macro to run demo program.

---
+++ example/demo/demo.C
@@ -0,0 +1,40 @@
+{
+// **************************************************************** 
+//  Example of Event display script. 
+// 
+//(Author) 
+//  10-Mar-1999 A.Miyamoto  Original version 
+//  21-Apr-1999 A.Miyamoto  Modified to run both in batch and interactive.
+//
+// $Id$
+//
+// **************************************************************** 
+
+  gROOT->LoadMacro("$JSFROOT/macro/GUIMainMacro.C");
+
+  if( strncmp(gSystem->HostName(),"ccjlc",5)  != 0 ) {
+    if( strcmp(gSystem->Getenv("OSTYPE"),"hpux") ==0 ) {
+      gSystem->Load("$JSFROOT/example/guiexam1/libJSFGUI.sl");
+    }
+    else {
+      gSystem->Load("$JSFROOT/example/guiexam1/libJSFGUI.so");
+    }
+  }
+  JSFGUIFrame *gui;
+  jsf  = new JSFSteer();
+
+  if( gClient == 0 ) {
+    gui=0;
+    BatchRun();
+  }
+  else  gui=new JSFGUIFrame(gClient->GetRoot(), 400, 220, kTRUE);
+
+  gui->RunDemo();
+//********************************************
+//*  Start execution
+//********************************************
+ 
+}
+
+
+
