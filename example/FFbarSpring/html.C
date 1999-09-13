0637c73cf3460aeab70e9a76537d906bdebe2235
Akiya Miyamoto <akiya.miyamoto@kek.jp>
1999-09-13 06:03:16 +0000

    13-September-1999   Some change in html.C

--- example/FFbarSpring/html.C
+++ example/FFbarSpring/html.C
@@ -1,5 +1,10 @@
 {
 
+  gEnv->SetValue("Root.Html.Root",
+               "http://www-jlc.kek.jp/subg/offl/jsf/RootHtmlDoc/html");
+  gEnv->SetValue("Root.Html.HomePage",
+                 "http://www-jlc.kek.jp/subg/offl/jsf/RootHtmlDoc/html");
+
   THtml html; 
 
     Char_t *name=gSystem->DynamicPathName("libFFbarSpring");
@@ -7,9 +12,6 @@
 
   html.SetOutputDir("../../html/"); 
   html.SetSourceDir("./:$(JSFROOT)/src:$(JSFROOT)/include:$(ROOTSYS)/src:$(ROOTSYS)/include"); 
-
-    Char_t *name=gSystem->DynamicPathName("libFFbarSpring");
-    gSystem->Load(name);
 
    html.MakeClass("FFbarSpring");
    html.MakeClass("FFbarSpringBuf");
@@ -19,16 +21,17 @@
 
    Char_t *cmd="cd ../../html ; mv USER_Index.html FFbarSpring_Index.html";
    gSystem->Exec(cmd);
+   gSystem->Exec("mkdir -p ../../html/examples/FFbarSpring");
    
 
-   Char_t *outdir="../../html/example/FFbarSpring";
+   Char_t *outdir="../../html/examples/FFbarSpring";
 
    //   html.Convert("Readme","Readme",outdir);
    html.Convert("sim.C","Spring example",outdir);
    html.Convert("nsim.C","Run second spring job",outdir);
    html.Convert("bases.C","Bases example",outdir);
-   html.Convert("anal.C","Bases example",outdir);
-   html.Convert("read.C","Bases example",outdir);
+   html.Convert("anal.C","Run Spring and analysis",outdir);
+   html.Convert("read.C","Example to read simulation data",outdir);
 
 
 }
