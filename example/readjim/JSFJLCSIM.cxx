0440f8f0f2260e452d2004ebf31ede40423e6582
Akiya Miyamoto <akiya.miyamoto@kek.jp>
2000-09-26 00:40:18 +0000

    26-September-2000 Akiya Miyamoto   Add KZGET2, KZGETH, and KZGETV

--- example/readjim/JSFJLCSIM.cxx
+++ example/readjim/JSFJLCSIM.cxx
@@ -18,6 +18,10 @@ ClassImp(JSFJLCSIM)
 
 extern "C" {
 extern void kzget_(char *cname, int *iseg, int *leng, int idat[],  int lenb);
+extern void kzget2_(char *cname, int *iseg, int *isseg, 
+		    int *leng, int idat[],  int lenb);
+extern void kzgeth_(int idat[]);
+extern void kzgetv_(char *cname,  int *ivers,  int lenb);
 };
 
 
@@ -26,6 +30,29 @@ void JSFJLCSIM::KZGET(char *cname, int iseg, int &leng, int idat[])
 {
   int lenb=strlen(cname);
   kzget_(cname, &iseg, &leng, idat, lenb);
+  return ;
+}
+
+// ---------------------------------------------------------------
+void JSFJLCSIM::KZGET2(char *cname, int iseg, int isseg, int &leng, int idat[])
+{
+  int lenb=strlen(cname);
+  kzget2_(cname, &iseg, &isseg, &leng, idat, lenb);
+  return ;
+}
+
+// ---------------------------------------------------------------
+void JSFJLCSIM::KZGETH(int idat[])
+{
+  kzgeth_(idat);
+  return ;
+}
+
+// ---------------------------------------------------------------
+void JSFJLCSIM::KZGETV(char *cname, int &ivers)
+{
+  int lenb=strlen(cname);
+  kzgetv_(cname, &ivers, lenb);
   return ;
 }
 
--- example/readjim/JSFJLCSIM.h
+++ example/readjim/JSFJLCSIM.h
@@ -19,6 +19,10 @@ class JSFJLCSIM
   virtual ~JSFJLCSIM(){}
 
   static void KZGET(char *cname, int iseg, int &leng, int idat[]);  
+  static void KZGET2(char *cname, int iseg, int isseg, 
+		     int &leng, int idat[]);  
+  static void KZGETH(int idat[]);  
+  static void KZGETV(char *cname, int &ivers);  
 
   ClassDef(JSFJLCSIM,0)  // Interfaces to JLCSIM routines.
 };
