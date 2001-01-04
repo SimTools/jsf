6ed409d3b2a98d3c65b1dda9ff1453365bef7987
Akiya Miyamoto <akiya.miyamoto@kek.jp>
2001-01-04 07:40:20 +0000

    4-Jan-2001 Akiya Miyamoto    Output  __ROOT_FULLVERSION__

--- macro/printversion.C
+++ macro/printversion.C
@@ -5,4 +5,6 @@
   printf("#define __ROOT_VERSION__ %d\n",ver);
   printf("#define __ROOT_MINORVERSION__ %d\n",mver);
   printf("#define __ROOT_PATCHLEVEL__ %d\n",plevel);
+  Int_t fulvrs=10000*ver+100*mver+plevel;
+  printf("#define __ROOT_FULLVERSION__ %d\n",fulvrs);
 }
