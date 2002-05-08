b643651d9309478fd5cdd357b1591baa3ba19eaa
Akiya Miyamoto <akiya.miyamoto@kek.jp>
2002-05-08 02:54:23 +0000

    8-May-2002 Akiya Miyamoto  Modifications to run with Pythia6 library

--- example/ZHStudy/zhsim.C
+++ example/ZHStudy/zhsim.C
@@ -21,16 +21,23 @@ TFile *file;
 // *****************************************
 void SetPythiaParameters()
 {
-  TPythia *tpy=py->GetPythia();
+  Int_t nvers=py->GetVersionNumber();
+
+  if( nvers >= 6 ) {
+    TPythia6 *tpy=py->GetPythia();
+  }
+  else {
+    TPythia *tpy=py->GetPythia();
+  }
   tpy->SetMSEL(18);                  // e+e- -> ZH process
 
   Int_t kf=25;                   // kf code for H0 is 25
-  Int_t rootver, rootmver, rootplevel;
-  sscanf(gROOT->GetVersion(),"%d.%d/%d",&rootver,&rootmver,&rootplevel);
-  if (rootver > 2 || (rootver == 2 && rootmver >= 23)) {
-     Int_t kc=tpy->Lucomp(kf);      // Get kc code for Higgs.
-  } else { 
-     Int_t kc=tpy->LuComp(kf);      // Get kc code for Higgs.
+  Int_t kc;
+  if( nvers >= 6 ) {
+    kc=tpy->Pycomp(kf);
+  }
+  else {
+    kc=tpy->Lucomp(kf);
   }
   tpy->SetPMAS(kc,1,fHiggsmass); // Set Higgs mass 
   printf(" Higgs mass parameters Mass=%g Width=%g Trancat %g LifeTime=%g\n",
