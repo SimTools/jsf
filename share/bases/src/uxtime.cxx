331291483fd227e0474a38870e7964085b94c76d
Fujii Keisuke <keisuke.fujii@kek.jp>
2002-10-06 12:50:54 +0000

    2002/10/06  K.Fujii	For gcc3.x

--- share/bases/src/uxtime.cxx
+++ share/bases/src/uxtime.cxx
@@ -10,6 +10,9 @@ float	uxtime()
 
 	times(&q);
         t = q.tms_utime;
-
+#if !defined __STRICT_ANSI__ && !defined __USE_XOPEN2K
 	return (float) t/ (float) CLK_TCK;
+#else
+	return (float) t/ (float) CLOCKS_PER_SEC;
+#endif
 }
