9723b3176fc19a1c702efd8c1294eac630720d84
Akiya Miyamoto <akiya.miyamoto@kek.jp>
2004-06-08 13:07:07 +0000

    8-June-2004 Akiya Miyamoto    Modified to build libG4all.a8-June-2004 Akiya Miyamoto    Modified to build libG4all.a8-June-2004 Akiya Miyamoto    Modified to build libG4all.a8-June-2004 Akiya Miyamoto    Modified to build libG4all.a8-June-2004 Akiya Miyamoto    Modified to build libG4all.a8-June-2004 Akiya Miyamoto    Modified to build libG4all.a8-June-2004 Akiya Miyamoto    Modified to build libG4all.a8-June-2004 Akiya Miyamoto    Modified to build libG4all.a

---
+++ example/jsfj4/g4merge.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo -n "G4INSTALL($G4INSTALL) directory .. "
+if [ ! -d $G4INSTALL ] ; then 
+  echo does not exist.
+  exit
+fi
+echo exist
+
+#
+echo -n "G4INSTALL/lib/G4SYSTEM($G4INSTALL/lib/$G4SYSTEM) directory .."
+if [ ! -d $G4INSTALL/lib/$G4SYSTEM ] ; then
+  echo does not exist.
+  exit 
+fi
+echo exist
+#
+
+WORKDIR=g4tmp
+rm -f $WORKDIR
+mkdir $WORKDIR
+cd $WORKDIR
+
+echo "Extractling Geant4 library objects"
+for f in $G4INSTALL/lib/$G4SYSTEM/lib*.a ; do
+  ar x $f
+done
+
+rm -f ../libG4all.a
+echo "Merging all Geant4 objects into single library"
+ar cr ../libG4all.a *.o
+rm -f $WORKDIR
