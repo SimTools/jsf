8356afcb53ac9a556f171bb7dd6790228387d456
Fujii Keisuke <keisuke.fujii@kek.jp>
2005-03-02 05:00:48 +0000

    2005/03/02 K.Fujii Integrate Pythia5&6 source files and removed jsfpythia5                    directory. Pythia5 plug-in can be built by setting                            export PYTHIAVERSOIN=5                    before building it.

--- src/lclibdep/jsfpythia6/ranmar.cxx
+++ src/lclibdep/jsfpythia6/ranmar.cxx
@@ -1,5 +1,16 @@
 #include "JSFRandom.h"
 
+#if __PYTHIA_VERSION__ <= 5
+extern "C" {
+  void pdfset_();
+  void structm_();
+  void dummy_(){ 
+	pdfset_(); 
+	structm_();
+  }
+};
+#endif
+
 extern "C" void ranmar_(float *rd, int *ndim)
 {
   for(Int_t i=0;i<*ndim;i++) {
