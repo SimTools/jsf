37aa4fe766abe2511b31d5409fd3e4b90b474180
Fujii Keisuke <keisuke.fujii@kek.jp>
2005-10-29 10:49:13 +0000

    2005/10/29 K.Fujii

--- macro/LoadLibraries.C
+++ macro/LoadLibraries.C
@@ -31,7 +31,8 @@ Int_t LoadLibraries(JSFEnv *env=0)
 
 // ** Load Satellites libraries, when SATELLITESROOT environment variable is defined.
 
-  if( strlen(gSystem->Getenv("SATELLITESROOT") ) > 3 ) {
+  if( gSystem->Getenv("SATELLITESROOT") &&
+      strlen(gSystem->Getenv("SATELLITESROOT") ) > 3 ) {
     FileStat_t filestat;
     Char_t *s4mdef=gSystem->ConcatFileName(gSystem->Getenv("SATELLITESROOT"),"macros/S4Macros.C");
     Char_t *s4m=env->GetValue("JSFJ4.S4MacroFileName",s4mdef);
